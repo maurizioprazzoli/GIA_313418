ddd9c0b4889fd808df5278dc5aaa964da491ff24
maurizioprazzoli <maurizio.prazzoli@gmail.com>
2021-03-11 23:24:42 +0100

    Update richiesta_1.sql

--- SQL/richiesta_1.sql
+++ SQL/richiesta_1.sql
@@ -5,9 +5,9 @@
 --
 SELECT		[Region].[Name]
 FROM		[Producer]		
-INNER JOIN	[Region]		    ON	[Producer].[RegionID]		= [Region].[ID]
+INNER JOIN	[Region]		ON	[Producer].[RegionID]		= [Region].[ID]
 INNER JOIN	[AttributeMap]	ON	[Producer].[Code]			= [AttributeMap].[ProducerCode]
-INNER JOIN	[Website]		    ON	[AttributeMap].[WebsiteID]	= [Website].[ID]
-INNER JOIN	[Attribute]		  ON	[Attribute].[ID]			= [AttributeMap].[AttributeID]
+INNER JOIN	[Website]		ON	[AttributeMap].[WebsiteID]	= [Website].[ID]
+INNER JOIN	[Attribute]		ON	[Attribute].[ID]			= [AttributeMap].[AttributeID]
 WHERE		[Website].[SiteName]	= 'website1.it'
 GROUP BY	[Region].[Name]
